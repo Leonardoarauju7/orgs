2b3a315001f7300354b8f2dda39d286f4c6afb54
Leonardo Araujo <leonardoearaujo@gmail.com>
2022-08-08 16:33:21 -0300

    Descrição e fontes

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StatusBar, SafeAreaView, Text } from "react-native"
+import { StatusBar, SafeAreaView, View } from "react-native"
 import React from "react"
 import Cesta from "./src/telas/cesta"
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat"
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
+  })
+
+  if (!fontCarregada) {
+    return <View /> // se a fonte nao for carregda vai carregar uma view em branco!
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from "react"
 
-import { StyleSheet, Image, Dimensions, Text } from "react-native"
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native"
 import topo from "../../assets/topo.png"
+import logo from "../../assets/logo.png"
 
 const width = Dimensions.get("screen").width
 
@@ -10,6 +11,21 @@ export default function Cesta() {
     <>
       <Image source={topo} style={estilos.topo} />
       <Text style={estilos.titulo}>Detalhe da cesta</Text>
+
+      <View style={estilos.cesta}>
+        <Text style={estilos.nome}>Cesta de Verduras</Text>
+
+        <View style={estilos.fazenda}>
+          <Image source={logo} style={estilos.logoFazenda} />
+          <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        </View>
+
+        <Text style={estilos.descricao}>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          para sua cozinha
+        </Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
+      </View>
     </>
   )
 }
@@ -29,5 +45,48 @@ const estilos = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     padding: 16,
+  },
+
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontFamily: "MontserratBold",
+  },
+
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+
+  logoFazenda: {
+    width: 32,
+    height: 32,
+  },
+
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratRegular",
+  },
+
+  descricao: {
+    color: "#a3a3a3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+
+  preco: {
+    color: "#2a9f85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
   },
 })
